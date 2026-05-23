483fdbb8bda666a597d8965ad443b0c89eba3927
josh <joshuapeddle128@gmail.com>
2026-05-23 13:02:45 -0230

    Add unit tests for GroundingConfig and GroundingContext

---
+++ tests/ReviewBot.Grounding.Tests/GroundingConfigTests.cs
@@ -0,0 +1,38 @@
+using FluentAssertions;
+using ReviewBot.Core.Domain;
+using ReviewBot.Grounding;
+
+namespace ReviewBot.Grounding.Tests;
+
+public class GroundingConfigTests
+{
+    [Fact]
+    public void DefaultGroundingConfigHasExpectedValues()
+    {
+        var d = GroundingConfig.Default;
+
+        d.Enabled.Should().BeTrue();
+        d.Build.Should().BeFalse();
+        d.Tests.Should().BeFalse();
+        d.BuildTimeoutSeconds.Should().Be(120);
+        d.TestTimeoutSeconds.Should().Be(300);
+        d.BuildCommand.Should().BeNull();
+        d.TestCommand.Should().BeNull();
+    }
+
+    [Fact]
+    public void ReviewConfigDefaultIncludesGroundingDefault()
+    {
+        ReviewConfig.Default.Grounding.Should().BeEquivalentTo(GroundingConfig.Default);
+    }
+
+    [Fact]
+    public void GroundingContextWithAllNullsIsValid()
+    {
+        var ctx = new GroundingContext(null, null, null);
+
+        ctx.Language.Should().BeNull();
+        ctx.Build.Should().BeNull();
+        ctx.Tests.Should().BeNull();
+    }
+}
